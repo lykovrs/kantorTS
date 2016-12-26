5163f78e1103798fb9f520fa414fdd24624a6bcc
Lykov Roman <lykovrs@gmail.com>
2016-12-26 14:44:21 +0300

    add strings example

---
+++ es-2015/strings/strings.js
@@ -0,0 +1,8 @@
+var str = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus repellat nesciunt inventore iste possimus iure debitis corporis sunt itaque aspernatur, mollitia vel, iusto eaque assumenda tenetur dolore, enim dignissimos et.";
+console.log(str.includes('dolor'));
+console.log(str.endsWith('lala'));
+console.log(str.startsWith('Lorem'));
+console.log('la'.repeat(3));
+var apples = 2;
+var oranges = 3;
+console.log(apples + " + " + oranges + " = " + (apples + oranges));
---
+++ es-2015/strings/strings.ts
@@ -0,0 +1,10 @@
+let str:string = `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus repellat nesciunt inventore iste possimus iure debitis corporis sunt itaque aspernatur, mollitia vel, iusto eaque assumenda tenetur dolore, enim dignissimos et.`
+console.log(str.includes('dolor'));
+console.log(str.endsWith('lala'));
+console.log(str.startsWith('Lorem'));
+console.log('la'.repeat(3));
+
+let apples:number = 2;
+let oranges:number = 3;
+
+console.log(`${apples} + ${oranges} = ${apples + oranges}`);
