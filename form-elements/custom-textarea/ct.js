b72ac4895aad3ad0d8cf61436725820c2a75578d
Lykov Roman <lykovrs@gmail.com>
2017-01-02 12:36:49 +0300

    add custom textarea

--- form-elements/custom-textarea/ct.js
+++ form-elements/custom-textarea/ct.js
@@ -1,8 +1,48 @@
 var CustomTextarea = (function () {
     function CustomTextarea() {
+        var _this = this;
+        this._area = document.getElementById('area');
+        this._view = document.getElementById('view');
+        // Коды клавиш для сочетаний
+        var PRESSED = {
+            edit: 69,
+            save: 83,
+            cancel: 27
+        };
         document.addEventListener('keydown', function (event) {
-            console.log(event.keyCode);
+            var ctrlKey = event.ctrlKey;
+            var secondKey = event.keyCode;
+            // Сочетание для редактирования поля
+            if (ctrlKey && secondKey == PRESSED.edit) {
+                event.preventDefault();
+                _this.editArea();
+            }
+            // Сочетание для сохранения результата ввода
+            if (ctrlKey && secondKey == PRESSED.save) {
+                event.preventDefault();
+                _this.saveResult();
+            }
+            // Нажатие клавиши для отмены
+            if (secondKey == PRESSED.cancel) {
+                event.preventDefault();
+                _this.cancel();
+            }
         });
     }
+    CustomTextarea.prototype.editArea = function () {
+        this._area.style.display = 'block';
+        this._view.style.display = 'none';
+        this._area.focus();
+    };
+    CustomTextarea.prototype.saveResult = function () {
+        this._value = this._area.value;
+        this._view.innerHTML = this._value;
+        this._area.style.display = 'none';
+        this._view.style.display = 'block';
+    };
+    CustomTextarea.prototype.cancel = function () {
+        this._area.style.display = 'none';
+        this._view.style.display = 'block';
+    };
     return CustomTextarea;
 }());
--- form-elements/custom-textarea/ct.ts
+++ form-elements/custom-textarea/ct.ts
@@ -1,12 +1,51 @@
 class CustomTextarea {
-    
+    private _area:HTMLElement = document.getElementById('area');
+    private _view:HTMLElement = document.getElementById('view');
+    private _value:string;
+
     constructor(){
+        // Коды клавиш для сочетаний
+        const PRESSED = {
+                edit: 69,
+                save: 83,
+                cancel: 27
+            }
         document.addEventListener('keydown', (event) => {
-            
-            console.log(event.keyCode)
-            
+            let ctrlKey = event.ctrlKey;
+            let secondKey = event.keyCode;
+            // Сочетание для редактирования поля
+            if(ctrlKey && secondKey == PRESSED.edit){
+               event.preventDefault();
+               this.editArea();
+            } 
+            // Сочетание для сохранения результата ввода
+            if (ctrlKey && secondKey == PRESSED.save) {
+               event.preventDefault();
+               this.saveResult();
+            }  
+            // Нажатие клавиши для отмены
+            if (secondKey == PRESSED.cancel) {
+               event.preventDefault();
+               this.cancel();
+            }
         })
     }
 
-  
+    editArea(){
+        this._area.style.display = 'block'; 
+        this._view.style.display = 'none';
+        this._area.focus();
+    } 
+
+    saveResult(){
+        this._value = this._area.value;
+        this._view.innerHTML = this._value;
+        this._area.style.display = 'none'; 
+        this._view.style.display = 'block';
+    }
+
+    cancel(){
+        this._area.style.display = 'none'; 
+        this._view.style.display = 'block';
+    }   
 }
