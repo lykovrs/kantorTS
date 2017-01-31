940aa5f9cc5f0119aa2962cc8a374f6711d8cc21
lykovrs <lykovrs@gmail.com>
2017-01-31 20:15:25 +0300

    add selecteble list

--- event-details/mouse-clicks/selectable-listls/selectable-list.js
+++ event-details/mouse-clicks/selectable-listls/selectable-list.js
@@ -1,14 +1,70 @@
 var SelectableList = (function () {
     function SelectableList(list) {
         var _this = this;
-        this.list = null;
-        this.list = list;
+        this.list = [];
+        this.selectedElements = [];
+        var items = list.querySelectorAll('li');
+        for (var i_1 = 0; i_1 < items.length; i_1++) {
+            this.list.push(items[i_1]);
+        }
+        /**
+         * Добавляем обработчик событий клика по пунктам меню
+         * @param  {String} 'click'
+         * @param  {Event}
+         */
         document.addEventListener('click', function (event) {
-            var element = event.target;
-            if (element.parentNode == _this.list) {
-                element.classList.add('selected');
+            var target = event.target;
+            // Обрабатываем события только по пунктам списка
+            if (target.parentNode === list) {
+                // Если зажата клавиша ctrl или cmd выделение не снимаем
+                if (event.ctrlKey || event.metaKey) {
+                    _this._select(target, list);
+                }
+                else if (event.shiftKey) {
+                    // Если зажата клавиша shift выделяем диапазон списка
+                    var starElementPosition_1 = _this.selectedElements.length - 1;
+                    var endElementPosition_1 = _this.list.indexOf(target);
+                    var itemPosition_1 = null;
+                    _this.list.forEach(function (item) {
+                        // debugger
+                        itemPosition_1 = _this.list.indexOf(item);
+                        if (starElementPosition_1 < endElementPosition_1 && itemPosition_1 >= starElementPosition_1 && itemPosition_1 <= endElementPosition_1) {
+                            _this._select(item, list);
+                        }
+                        if (starElementPosition_1 > endElementPosition_1 && itemPosition_1 <= starElementPosition_1 && itemPosition_1 >= endElementPosition_1) {
+                            _this._select(item, list);
+                        }
+                    });
+                    console.log(starElementPosition_1, endElementPosition_1);
+                }
+                else {
+                    // В остальных случаях снимаем выделение и чистим выбранные значения
+                    if (_this.selectedElements.length) {
+                        _this._clearSelected();
+                    }
+                    _this._select(target, list);
+                }
             }
         });
     }
+    /**
+     * Устанавливаем выделение и добавляем элемент в список выбранных
+     * @param {HTMLLIElement} element Элемент, на который будет установлено выделение
+     */
+    SelectableList.prototype._select = function (element, list) {
+        if (element.parentNode == list) {
+            element.classList.add('selected');
+            this.selectedElements.push(element);
+        }
+    };
+    /**
+     * Снимаем выделение с выбранных элементов и чистим очередь
+     */
+    SelectableList.prototype._clearSelected = function () {
+        this.selectedElements.forEach(function (item) {
+            item.classList.remove('selected');
+        });
+        this.selectedElements.length = 0;
+    };
     return SelectableList;
 }());
--- event-details/mouse-clicks/selectable-listls/selectable-list.ts
+++ event-details/mouse-clicks/selectable-listls/selectable-list.ts
@@ -1,14 +1,74 @@
-
 class SelectableList {
-  public list:any = null;
+    public list: HTMLLIElement[] = [];
+    protected selectedElements: HTMLLIElement[] = [];
 
-  constructor(list:any){
-    this.list = list;
-    document.addEventListener('click', (event:any) => {
-      let element = event.target;
-      if(element.parentNode == this.list) {
-        element.classList.add('selected') 
-      }
-    });
-  }
+    constructor(list: HTMLUListElement) {
+        let items = list.querySelectorAll('li');
+        for (let i = 0; i < items.length; i++) {
+            this.list.push(items[i]);
+        }
+
+        /**
+         * Добавляем обработчик событий клика по пунктам меню
+         * @param  {String} 'click'
+         * @param  {Event}
+         */
+        document.addEventListener('click', (event: MouseEvent) => {
+            let target: any = event.target;
+            // Обрабатываем события только по пунктам списка
+            if (target.parentNode === list) {
+                // Если зажата клавиша ctrl или cmd выделение не снимаем
+                if (event.ctrlKey || event.metaKey) {
+                    this._select(target, list);
+                } else if (event.shiftKey) {
+                    // Если зажата клавиша shift выделяем диапазон списка
+                    let starElementPosition = this.selectedElements.length - 1;
+                    let endElementPosition = this.list.indexOf(target);
+                    let itemPosition = null;
+                    this.list.forEach(item => {
+                        // debugger
+                        itemPosition = this.list.indexOf(item);
+                        if (starElementPosition < endElementPosition && itemPosition >= starElementPosition && itemPosition <= endElementPosition) {
+                            this._select(item, list);
+                        }
+                        if (starElementPosition > endElementPosition && itemPosition <= starElementPosition && itemPosition >= endElementPosition) {
+                            this._select(item, list);
+                        }
+                    })
+
+
+
+                    console.log(starElementPosition, endElementPosition)
+
+
+
+                } else {
+                    // В остальных случаях снимаем выделение и чистим выбранные значения
+                    if (this.selectedElements.length) {
+                        this._clearSelected();
+                    }
+                    this._select(target, list);
+                }
+            }
+        });
+    }
+    /**
+     * Устанавливаем выделение и добавляем элемент в список выбранных
+     * @param {HTMLLIElement} element Элемент, на который будет установлено выделение
+     */
+    private _select(element: HTMLLIElement, list: HTMLUListElement): void {
+        if (element.parentNode == list) {
+            element.classList.add('selected');
+            this.selectedElements.push(element);
+        }
+    }
+    /**
+     * Снимаем выделение с выбранных элементов и чистим очередь
+     */
+    private _clearSelected(): void {
+        this.selectedElements.forEach((item) => {
+            item.classList.remove('selected');
+        })
+        this.selectedElements.length = 0;
+    }
 }
