f30b7b52047f9d8727e61d0184ec82fd377b47d6
lykovrs <lykovrs@gmail.com>
2017-02-04 00:53:27 +0300

    add mouse event task

--- event-details/mousemove-mouseover-mouseout-mouseenter-mouseleave/behavior-nested-tooltip/behavior-nested-tooltip.js
+++ event-details/mousemove-mouseover-mouseout-mouseenter-mouseleave/behavior-nested-tooltip/behavior-nested-tooltip.js
@@ -1,5 +1,30 @@
 var BehaviorNestedTooltip = (function () {
     function BehaviorNestedTooltip() {
+        var _this = this;
+        this._template = document.createElement('div');
+        document.body.appendChild(this._template);
+        this._template.classList.add('tooltip');
+        document.addEventListener('mouseover', function (event) {
+            var currentElement = event.target;
+            var tooltip = currentElement.dataset.tooltip;
+            _this._template.innerText = tooltip;
+            if (tooltip) {
+                _this.showTemplate();
+            }
+        });
+        document.addEventListener('mouseout', function (event) {
+            var currentElement = event.target;
+            var tooltip = currentElement.dataset.tooltip;
+            if (tooltip) {
+                _this.hideTemplate();
+            }
+        });
     }
+    BehaviorNestedTooltip.prototype.showTemplate = function () {
+        this._template.style.display = 'block';
+    };
+    BehaviorNestedTooltip.prototype.hideTemplate = function () {
+        this._template.style.display = 'none';
+    };
     return BehaviorNestedTooltip;
 }());
--- event-details/mousemove-mouseover-mouseout-mouseenter-mouseleave/behavior-nested-tooltip/behavior-nested-tooltip.ts
+++ event-details/mousemove-mouseover-mouseout-mouseenter-mouseleave/behavior-nested-tooltip/behavior-nested-tooltip.ts
@@ -1,7 +1,37 @@
 class BehaviorNestedTooltip {
-  protected _tooltips:NodeList
+    private _tooltips: NodeList;
+    private _template: HTMLDivElement = document.createElement('div');
 
-  constructor() {
-  
-  }
+
+    constructor() {
+        document.body.appendChild(this._template);
+        this._template.classList.add('tooltip');
+
+        document.addEventListener('mouseover', (event: Event) => {
+            let currentElement = event.target;
+            let tooltip = currentElement.dataset.tooltip;
+            this._template.innerText = tooltip;
+
+            if (tooltip) {
+                this.showTemplate();
+            }
+        })
+
+        document.addEventListener('mouseout', (event: Event) => {
+            let currentElement = event.target;
+            let tooltip = currentElement.dataset.tooltip;
+
+            if (tooltip) {
+                this.hideTemplate();
+            }
+        })
+    }
+
+    showTemplate() {
+        this._template.style.display = 'block';
+    }
+
+    hideTemplate() {
+        this._template.style.display = 'none';
+    }
 }
