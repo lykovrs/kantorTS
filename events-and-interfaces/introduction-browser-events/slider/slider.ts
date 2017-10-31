2ae43ca05cd3cd895869468a2a149c53e2ec0d0b
Roman Lykov <lykovrs@gmail.com>
2017-10-31 11:21:09 +0300

    add types for slider

--- events-and-interfaces/introduction-browser-events/slider/slider.ts
+++ events-and-interfaces/introduction-browser-events/slider/slider.ts
@@ -1,52 +1,54 @@
 class Slider {
-    private _collection = null;
-    private _leftArrow = null;
-    private _rightArrow = null;
-    private _visibleWidth = null;
-    private _wrapperItems = null;
-    private _totalLeft = 0;
-   
-    constructor(slider){
-        if(slider){
-            this._rightArrow = slider.getElementsByClassName('arrow-right')[0];
-            this._leftArrow = slider.getElementsByClassName('arrow-left')[0];
-            
-            this._rightArrow.addEventListener('click', (slider)=> {
-                this.nextSlide()
-            });
-            this._leftArrow.addEventListener('click', (slider)=> {
-                this.prevSlide()
-            });           
-            this._visibleWidth = slider.offsetWidth;
-            this._collection = slider.getElementsByTagName('li')
-            this._wrapperItems = slider.querySelector('ul');
-            this._wrapperItems.style.width = this.getWidthCollection() + 'px';
-        }        
-    };
-    
-    nextSlide():void {
-        if(this._totalLeft - this._visibleWidth >= -this.getWidthCollection()){
-            this._wrapperItems.style.left = `${this._totalLeft += -this._visibleWidth}px`;
-        } else {
-            this._totalLeft = 0;
-            this._wrapperItems.style.left = 0 + 'px'
-        }
-    };
+  private _collection: NodeListOf<HTMLElement> = null;
+  private _leftArrow: Element = null;
+  private _rightArrow: Element = null;
+  private _visibleWidth: number = null;
+  private _wrapperItems: HTMLElement = null;
+  private _totalLeft = 0;
 
-    prevSlide():void {
-        if(this._totalLeft < 0){
-            this._wrapperItems.style.left = `${this._totalLeft += this._visibleWidth}px`;
-        } else {
-            this._totalLeft = -this.getWidthCollection() + this._visibleWidth;
-            this._wrapperItems.style.left = -this.getWidthCollection() + this._visibleWidth + 'px'
-        }
-    };
+  constructor(slider: HTMLElement) {
+    if (slider) {
+      this._rightArrow = slider.getElementsByClassName("arrow-right")[0];
+      this._leftArrow = slider.getElementsByClassName("arrow-left")[0];
 
-    getWidthCollection():number {
-        let widthCollection = 0;
-        for(let item of this._collection){
-           widthCollection += item.offsetWidth;
-        }
-        return widthCollection;
-    };
+      this._rightArrow.addEventListener("click", slider => {
+        this.nextSlide();
+      });
+      this._leftArrow.addEventListener("click", slider => {
+        this.prevSlide();
+      });
+      this._visibleWidth = slider.offsetWidth;
+      this._collection = slider.getElementsByTagName("li");
+      this._wrapperItems = slider.querySelector("ul");
+      this._wrapperItems.style.width = this.getWidthCollection() + "px";
+    }
+  }
+
+  nextSlide(): void {
+    if (this._totalLeft - this._visibleWidth >= -this.getWidthCollection()) {
+      this._wrapperItems.style.left = `${(this._totalLeft += -this
+        ._visibleWidth)}px`;
+    } else {
+      this._totalLeft = 0;
+      this._wrapperItems.style.left = 0 + "px";
+    }
+  }
+
+  prevSlide(): void {
+    if (this._totalLeft < 0) {
+      this._wrapperItems.style.left = `${(this._totalLeft += this._visibleWidth)}px`;
+    } else {
+      this._totalLeft = -this.getWidthCollection() + this._visibleWidth;
+      this._wrapperItems.style.left =
+        -this.getWidthCollection() + this._visibleWidth + "px";
+    }
+  }
+
+  getWidthCollection(): number {
+    let widthCollection = 0;
+    for (let item of this._collection) {
+      widthCollection += item.offsetWidth;
+    }
+    return widthCollection;
+  }
 }
